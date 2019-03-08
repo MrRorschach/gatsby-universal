9f3c891187fdcab1561fe6d54a5e3b135442d512
Robert Foster <robert.foster@austincc.edu>
2019-03-08 14:33:56 -0600

    Added images and built

--- site-config.js
+++ site-config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Robert's Portfolio`,
-  siteTitleShort: `RobPort`,
-  siteDescription: `Robert's Portfolio`,
-  siteUrl: ``,
-  themeColor: `#000`,
-  backgroundColor: `#fff`,
+  siteTitle: "Robert's Portfolio",
+  siteTitleShort: 'RobPort',
+  siteDescription: "Robert's Portfolio",
+  siteUrl: 'https://robertfoster.dev',
+  themeColor: '#000',
+  backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
+    twitter: 'gatsbyjs',
+    fbAppId: '966242223397117',
   },
 };
