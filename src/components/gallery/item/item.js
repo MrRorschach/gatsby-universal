56fe211e46cf7aa131b1c492bfae82845c77d9b4
Robert Foster <robert.foster@austincc.edu>
2019-06-07 12:29:55 -0500

    Added img border radius styling

--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import { Title, Copy, Container, A } from './item.css';
 
+const round = {
+  borderRadius: '2px 2px 0px 0px',
+};
 const Item = ({ title, copy, image, link }) => (
   <Container>
-    <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
+    <Img
+      style={round}
+      fluid={image ? image.childImageSharp.fluid : {}}
+      alt={title}
+    />
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
